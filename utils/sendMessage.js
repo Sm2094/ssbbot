3858cfb57bafd5e7e9fb48b747d70d8223f3663f
Sm2094 <sm2023bis28@gmail.com>
2026-03-18 12:19:23 +0200

    fix railway deploy

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function sendMessage(to, text) {
 
   await axios.post(
-    `https://graph.facebook.com/v19.0/${PHONE_NUMBER_ID}/messages`,
+    `https://graph.facebook.com/v19.0/${process.env.PHONE_NUMBER_ID}/messages`,
     {
       messaging_product: "whatsapp",
       to: to,
