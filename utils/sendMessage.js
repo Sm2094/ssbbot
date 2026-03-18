93ab39d0e0e3e1ca16e30be26ed99d2fbfb599ff
Sm2094 <sm2023bis28@gmail.com>
2026-03-18 10:35:14 +0200

    fix railway deploy

--- utils/sendMessage.js
+++ utils/sendMessage.js
@@ -3,15 +3,15 @@ const axios = require('axios');
 async function sendMessage(to, text) {
 
   await axios.post(
-    `https://graph.facebook.com/v19.0/YOUR_PHONE_NUMBER_ID/messages`,
+    `https://graph.facebook.com/v19.0/1083279688194277/messages`,
     {
       messaging_product: "whatsapp",
-      to,
+      to: "27833531131",
       text: { body: text }
     },
     {
       headers: {
-        Authorization: `Bearer YOUR_META_TOKEN`
+        Authorization: `Bearer EAARZCEVQbdl8BQ9gbq7HZAiB1Temew0SaSe4LLEGKxqx04qRGGfVnBsnGGZBrkE0ZC7IMm3x3iFk5i8IEg7ZCIlum5ONUg0XlVA8sWgq36mYbQZBjk7ZCvGB9SLcG0ogR5bwyjDt6RWxjuIAUwLGbDTkZCPIkYE7d3rb61xuh4HiLGYnf7aWUHYL1VTNyUZAU1UcXJq2Fjsx6YCBCRchRfnAl3xTwfeAIWZBTq2Gd7ZBX0Tb9p6fAmPx7AhbHCtsE3G2nNesSLQvExOxdJoTkc31SLjfAs1`
       }
     }
   );
