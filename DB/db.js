d700ea0e0ef9d3faac8f5e026fb3975c339caa46
Sm2094 <sm2023bis28@gmail.com>
2026-03-18 14:03:56 +0200

    fix railway deploy

--- DB/db.js
+++ DB/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
+  host: process.env.HOST,
+  user: process.env.USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DATABASE
 });
 
 module.exports = db;
